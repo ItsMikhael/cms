6ffdfb79f7777763a17630a6916c6f814e16aae9
ItsMikhael <mstefanski.entrepreneur@gmail.com>
2020-08-21 14:00:01 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+include 'includes/database.php';
+
+$sql = "SELECT *
+        FROM article
+        ORDER BY id";
+
+$results = mysqli_query($conn, $sql);
+
+if($results == false) {
+    echo mysqli_error($conn);
+} else {
+    $articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
+}
+
+?>
+
+<?php require 'includes/header.php'; ?>
+
+    <ul>
+        <?php foreach ($articles as $article): ?>
+            <li>
+                <article>
+                    <h2><a href="article.php?id=<?= $article['id']; ?>"><?= $article['title']; ?></a></h2>
+                    <p><?= $article['content']; ?></p>
+                </article>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+
+<?php require 'includes/footer.php'; ?>
